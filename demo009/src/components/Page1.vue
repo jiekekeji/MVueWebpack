76b638d90b77ae0241faf60978bbe4b29f4a1358
jiekekeji <jieke_rose@foxmail.com>
2017-01-24 09:35:05 +0800

    Vuex  Mutations 初识

---
+++ demo009/src/components/Page1.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="content">
+        这是page1组件 msg={{listenCount}}
+        <p>名字：{{stu.name}} 年龄:{{stu.age}}</p>
+    </div>
+</template>
+<style scoped>
+    .content {
+        width: 100%;
+        height: 100%;
+        margin: 0 auto;
+        line-height: 120px;
+        text-align: center;
+        background-color: azure;
+    }
+</style>
+<script>
+    import store from '../store/store';
+
+    export default{
+        data(){
+            return {
+                msg: store.state.count,
+                stu: store.state.stu,
+            }
+        },
+        components: {},
+        computed: {
+            listenCount () {
+                console.log('状态值发生改变:' + store.state.count);
+                return store.state.count
+            },
+        },
+        methods: {
+            skip2Page2: function () {
+                this.$router.push({path: 'Page2'});
+            }
+        },
+        created () {
+        },
+    }
+</script>
