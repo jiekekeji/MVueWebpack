b226746296313c17c3ab4a67444dae5e351162c2
jiekekeji <jieke123>
2017-02-10 18:57:26 +0800

    Vuex Mutations 初识

--- demo009/build/webpack.base.conf.js
+++ demo009/build/webpack.base.conf.js
@@ -1,77 +1,4 @@
 var path = require('path')
-<<<<<<< HEAD
-var utils = require('./utils')
-var config = require('../config')
-var vueLoaderConfig = require('./vue-loader.conf')
-
-function resolve (dir) {
-  return path.join(__dirname, '..', dir)
-}
-
-module.exports = {
-  entry: {
-    app: './src/main.js'
-  },
-  output: {
-    path: config.build.assetsRoot,
-    filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
-  },
-  resolve: {
-    extensions: ['.js', '.vue', '.json'],
-    modules: [
-      resolve('src'),
-      resolve('node_modules')
-    ],
-    alias: {
-      'vue$': 'vue/dist/vue.common.js',
-      'src': resolve('src'),
-      'assets': resolve('src/assets'),
-      'components': resolve('src/components')
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        enforce: "pre",
-        include: [resolve('src'), resolve('test')],
-        options: {
-          formatter: require('eslint-friendly-formatter')
-        }
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: vueLoaderConfig
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        query: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        query: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
-      }
-    ]
-  }
-=======
 var config = require('../config')
 var utils = require('./utils')
 var projectRoot = path.resolve(__dirname, '../')
@@ -84,93 +11,92 @@ var cssSourceMapProd = (env === 'production' && config.build.productionSourceMap
 var useCssSourceMap = cssSourceMapDev || cssSourceMapProd
 
 module.exports = {
-    entry: {
-        app: './src/main.js'
-    },
-    output: {
-        path: config.build.assetsRoot,
-        publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
-        filename: '[name].js'
-    },
-    resolve: {
-        extensions: ['', '.js', '.vue', '.json'],
-        fallback: [path.join(__dirname, '../node_modules')],
-        alias: {
-            'vue$': 'vue/dist/vue.common.js',
-            'src': path.resolve(__dirname, '../src'),
-            'assets': path.resolve(__dirname, '../src/assets'),
-            'components': path.resolve(__dirname, '../src/components')
-        }
-    },
-    resolveLoader: {
-        fallback: [path.join(__dirname, '../node_modules')]
-    },
-    module: {
-        preLoaders: [
-            //代码规范暂时去掉
-            // {
-            //     test: /\.vue$/,
-            //     loader: 'eslint',
-            //     include: [
-            //         path.join(projectRoot, 'src')
-            //     ],
-            //     exclude: /node_modules/
-            // },
-            // {
-            //     test: /\.js$/,
-            //     loader: 'eslint',
-            //     include: [
-            //         path.join(projectRoot, 'src')
-            //     ],
-            //     exclude: /node_modules/
-            // }
-        ],
-        loaders: [
-            {
-                test: /\.vue$/,
-                loader: 'vue'
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel',
-                include: [
-                    path.join(projectRoot, 'src')
-                ],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.json$/,
-                loader: 'json'
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                loader: 'url',
-                query: {
-                    limit: 10000,
-                    name: utils.assetsPath('img/[name].[hash:7].[ext]')
-                }
-            },
-            {
-                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-                loader: 'url',
-                query: {
-                    limit: 10000,
-                    name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-                }
-            }
-        ]
-    },
-    //代码规范暂时去掉
-    // eslint: {
-    //   formatter: require('eslint-friendly-formatter')
-    // },
-    vue: {
-        loaders: utils.cssLoaders({sourceMap: useCssSourceMap}),
-        postcss: [
-            require('autoprefixer')({
-                browsers: ['last 2 versions']
-            })
-        ]
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    path: config.build.assetsRoot,
+    publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.vue', '.json'],
+    fallback: [path.join(__dirname, '../node_modules')],
+    alias: {
+      'vue$': 'vue/dist/vue.common.js',
+      'src': path.resolve(__dirname, '../src'),
+      'assets': path.resolve(__dirname, '../src/assets'),
+      'components': path.resolve(__dirname, '../src/components')
     }
->>>>>>> origin/master
+  },
+  resolveLoader: {
+    fallback: [path.join(__dirname, '../node_modules')]
+  },
+  module: {
+    preLoaders: [
+      //代码规范暂时去掉
+      // {
+      //     test: /\.vue$/,
+      //     loader: 'eslint',
+      //     include: [
+      //         path.join(projectRoot, 'src')
+      //     ],
+      //     exclude: /node_modules/
+      // },
+      // {
+      //     test: /\.js$/,
+      //     loader: 'eslint',
+      //     include: [
+      //         path.join(projectRoot, 'src')
+      //     ],
+      //     exclude: /node_modules/
+      // }
+    ],
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        include: [
+          path.join(projectRoot, 'src')
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  },
+  //代码规范暂时去掉
+  // eslint: {
+  //   formatter: require('eslint-friendly-formatter')
+  // },
+  vue: {
+    loaders: utils.cssLoaders({sourceMap: useCssSourceMap}),
+    postcss: [
+      require('autoprefixer')({
+        browsers: ['last 2 versions']
+      })
+    ]
+  }
 }
--- demo009/config/index.js
+++ demo009/config/index.js
@@ -14,24 +14,11 @@ module.exports = {
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
     productionGzip: false,
-<<<<<<< HEAD
-    productionGzipExtensions: ['js', 'css'],
-    // Run the build command with an extra argument to
-    // View the bundle analyzer report after build finishes:
-    // `npm run build --report`
-    // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
-=======
     productionGzipExtensions: ['js', 'css']
->>>>>>> origin/master
   },
   dev: {
     env: require('./dev.env'),
     port: 8080,
-<<<<<<< HEAD
-    autoOpenBrowser: true,
-=======
->>>>>>> origin/master
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
