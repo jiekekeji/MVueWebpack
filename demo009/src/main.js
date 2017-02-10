b77e7c125524b7f3197ad891cbdae56e98f9af79
jiekekeji <jieke123>
2017-02-10 18:56:31 +0800

    Vuex Mutations 初识

--- demo009/src/main.js
+++ demo009/src/main.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
-})
-=======
 // src/main.js
 //导入这两个包
 import Vue from 'vue'
@@ -22,8 +7,7 @@ import router from './router/router'
 
 // 实例化我们的Vue
 var app = new Vue({
-    el: '#app',
-    router,
-    ...App,
+  el: '#app',
+  router,
+  ...App,
 });
->>>>>>> origin/master
