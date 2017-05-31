17ae9b556173755314f1f60814fe50df4d70d710
jiekekeji <jieke_rose@foxmail.com>
2017-05-31 17:15:25 +0800

    vue-render函数

---
+++ demo021/src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <render-demo></render-demo>
+  </div>
+</template>
+
+<script>
+  import demo from "../render/demo"
+  export default {
+    data () {
+      return {}
+    },
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ demo021/src/render/demo.js
@@ -0,0 +1,8 @@
+import Vue from "vue"
+
+Vue.component("render-demo", {
+  render: function (createElement) {
+
+    return createElement();
+  }
+});
